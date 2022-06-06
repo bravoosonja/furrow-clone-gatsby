96a98322ef96743a4b8cc9c5d5a3d59b2fcf10ea
Bravoo Sonja <bravoosonja@gmail.com>
2022-06-06 11:03:59 +1000

    complete file structure, start nav

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,20 @@
 module.exports = {
   siteMetadata: {
     title: `furrow clone gatsby`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+  plugins: [
+    "gatsby-plugin-styled-components",
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/assets/images/",
+      },
+      __key: "images",
     },
-    __key: "images"
-  }]
+  ],
 };
