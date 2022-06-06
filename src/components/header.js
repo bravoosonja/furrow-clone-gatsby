0d00db33b6fc8913a397d8ae80a2219d231da6df
Bravoo Sonja <bravoosonja@gmail.com>
2022-06-06 13:05:15 +1000

    add animation on the navbar using framer motion

--- src/components/header.js
+++ src/components/header.js
@@ -29,7 +29,11 @@ const Header = () => {
   }, [currentTheme]);
 
   return (
-    <HeaderNav>
+    <HeaderNav
+      animate={{ y: 0, opacity: 1 }}
+      initial={{ y: -72, opacity: 1 }}
+      transition={{ duration: 1, ease: [0.6, 0.05, -0.01, 0.9] }}
+    >
       <Container fluid>
         {console.log(currentTheme)}
         <Flex spaceBetween noHeight>
